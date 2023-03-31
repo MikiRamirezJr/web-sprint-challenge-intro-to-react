7eb568e9ae9990c6a97110d615d010b9e055dc4a
Miguel Ramirez <miki1752@gmail.com>
2023-03-31 18:00:41 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Character from './components/Character';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2rem;
+`;
 
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(response => {
+        console.log(response.data);
+        setCharacters(response.data);
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <Container>
+         {characters.map(character => (
+          <Character character = { character } key={character.name} />
+        
+         )) }
+
+        </Container>
+      )}
+    </>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+function Character({ character }) {
+  return (
+    <Wrapper>
+      <h1> {character.name} </h1>
+      <p> {character.height} </p>
+      <p> {character.mass }</p>
+      <p> {character.hair_color}</p>
+      <p> {character.skin_color} </p>
+    </Wrapper>
+  );
+}
+
+export default Character;
